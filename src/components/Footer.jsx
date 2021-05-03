0a9305a50df92b46cadbe49aad0a528a228cd073
pengtikui <949828390@qq.com>
2021-05-03 22:18:05 +0800

    WIP: Add basic components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="max-w-screen-md mx-auto pt-8 pb-6">
+        <p className="text-gray-600 text-sm">
+          Made by{' '}
+          <a
+            className="underline hover:text-gray-800"
+            href="https://pengtikui.cn"
+            target="_blank"
+          >
+            Paranoid_K
+          </a>
+          <span> · </span>
+          Powered by{' '}
+          <a
+            className="underline hover:text-gray-800"
+            href="https://vercel.com"
+            target="_blank"
+          >
+            Vercel
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
